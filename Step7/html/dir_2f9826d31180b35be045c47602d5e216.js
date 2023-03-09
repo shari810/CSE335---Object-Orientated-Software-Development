63c3ba8f146ef3338287a44b4e922e442a7e840a
shari810 <63125461+shari810@users.noreply.github.com>
2023-03-09 09:23:38 -0500

    Add files via upload

---
+++ Step7/html/dir_2f9826d31180b35be045c47602d5e216.js
@@ -0,0 +1,56 @@
+var dir_2f9826d31180b35be045c47602d5e216 =
+[
+    [ "Actor.cpp", "_actor_8cpp.html", null ],
+    [ "Actor.h", "_actor_8h.html", [
+      [ "Actor", "class_actor.html", "class_actor" ]
+    ] ],
+    [ "Drawable.cpp", "_drawable_8cpp.html", null ],
+    [ "Drawable.h", "_drawable_8h.html", [
+      [ "Drawable", "class_drawable.html", "class_drawable" ]
+    ] ],
+    [ "HaroldFactory.cpp", "_harold_factory_8cpp.html", null ],
+    [ "HaroldFactory.h", "_harold_factory_8h.html", [
+      [ "HaroldFactory", "class_harold_factory.html", "class_harold_factory" ]
+    ] ],
+    [ "HeadTop.cpp", "_head_top_8cpp.html", null ],
+    [ "HeadTop.h", "_head_top_8h.html", [
+      [ "HeadTop", "class_head_top.html", "class_head_top" ]
+    ] ],
+    [ "ImageDrawable.cpp", "_image_drawable_8cpp.html", null ],
+    [ "ImageDrawable.h", "_image_drawable_8h.html", [
+      [ "ImageDrawable", "class_image_drawable.html", "class_image_drawable" ]
+    ] ],
+    [ "MainFrame.cpp", "_main_frame_8cpp.html", "_main_frame_8cpp" ],
+    [ "MainFrame.h", "_main_frame_8h.html", [
+      [ "MainFrame", "class_main_frame.html", "class_main_frame" ]
+    ] ],
+    [ "Picture.cpp", "_picture_8cpp.html", null ],
+    [ "Picture.h", "_picture_8h.html", [
+      [ "Picture", "class_picture.html", "class_picture" ],
+      [ "Picture::Iter", "class_picture_1_1_iter.html", "class_picture_1_1_iter" ]
+    ] ],
+    [ "PictureFactory.cpp", "_picture_factory_8cpp.html", null ],
+    [ "PictureFactory.h", "_picture_factory_8h.html", [
+      [ "PictureFactory", "class_picture_factory.html", "class_picture_factory" ]
+    ] ],
+    [ "PictureObserver.cpp", "_picture_observer_8cpp.html", null ],
+    [ "PictureObserver.h", "_picture_observer_8h.html", [
+      [ "PictureObserver", "class_picture_observer.html", "class_picture_observer" ]
+    ] ],
+    [ "PolyDrawable.cpp", "_poly_drawable_8cpp.html", null ],
+    [ "PolyDrawable.h", "_poly_drawable_8h.html", [
+      [ "PolyDrawable", "class_poly_drawable.html", "class_poly_drawable" ]
+    ] ],
+    [ "SpartyFactory.cpp", "_sparty_factory_8cpp.html", null ],
+    [ "SpartyFactory.h", "_sparty_factory_8h.html", [
+      [ "SpartyFactory", "class_sparty_factory.html", "class_sparty_factory" ]
+    ] ],
+    [ "ViewEdit.cpp", "_view_edit_8cpp.html", "_view_edit_8cpp" ],
+    [ "ViewEdit.h", "_view_edit_8h.html", [
+      [ "ViewEdit", "class_view_edit.html", "class_view_edit" ]
+    ] ],
+    [ "ViewTimeline.cpp", "_view_timeline_8cpp.html", null ],
+    [ "ViewTimeline.h", "_view_timeline_8h.html", [
+      [ "ViewTimeline", "class_view_timeline.html", "class_view_timeline" ]
+    ] ]
+];
---
+++ Step7/html/files_dup.js
@@ -0,0 +1,9 @@
+var files_dup =
+[
+    [ "CanadianExperienceLib", "dir_2f9826d31180b35be045c47602d5e216.html", "dir_2f9826d31180b35be045c47602d5e216" ],
+    [ "CanadianExperienceApp.cpp", "_canadian_experience_app_8cpp.html", null ],
+    [ "CanadianExperienceApp.h", "_canadian_experience_app_8h.html", [
+      [ "CanadianExperienceApp", "class_canadian_experience_app.html", "class_canadian_experience_app" ]
+    ] ],
+    [ "main.cpp", "main_8cpp.html", null ]
+];
