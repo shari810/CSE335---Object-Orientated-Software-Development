ae2f84825dfbfccc3941d49d0cd2a48eac514fb5
shari810 <63125461+shari810@users.noreply.github.com>
2023-03-09 08:56:57 -0500

    Add files via upload

---
+++ step2/Aquarium/cmake-build-debug/AquariumLib/CMakeFiles/AquariumLib.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "AquariumLib.lib"
+  "AquariumLib.pdb"
+  "CMakeFiles/AquariumLib.dir/cmake_pch.cxx.pch"
+  "CMakeFiles/AquariumLib.dir/Aquarium.cpp.obj"
+  "CMakeFiles/AquariumLib.dir/AquariumView.cpp.obj"
+  "CMakeFiles/AquariumLib.dir/Item.cpp.obj"
+  "CMakeFiles/AquariumLib.dir/MainFrame.cpp.obj"
+  "CMakeFiles/AquariumLib.dir/cmake_pch.cxx.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AquariumLib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
