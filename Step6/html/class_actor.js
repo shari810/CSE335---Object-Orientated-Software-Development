fb8d21f0ff82e2e8913bce3237fa6c5a5fca1eb6
shari810 <63125461+shari810@users.noreply.github.com>
2023-03-09 09:15:50 -0500

    Add files via upload

---
+++ Step6/html/class_actor.js
@@ -0,0 +1,28 @@
+var class_actor =
+[
+    [ "~Actor", "class_actor.html#ad807fe8f85e72ab263a0c05e3231cb39", null ],
+    [ "Actor", "class_actor.html#a5ae4ade0cf177d596d2a740b6d604a55", null ],
+    [ "Actor", "class_actor.html#a22f32e3ce430a9f5c6dc9eab2ed7224c", null ],
+    [ "Actor", "class_actor.html#aa7adeebfd43b734632e98ec192dead46", null ],
+    [ "AddDrawable", "class_actor.html#a8e34433ee1853778ddcacd0cec41cd08", null ],
+    [ "Draw", "class_actor.html#a5642864dfc751a14605fc0a5b6983790", null ],
+    [ "GetName", "class_actor.html#ad250f73ec316d1097c070168ece2299a", null ],
+    [ "GetPicture", "class_actor.html#aaf64e450a6f6f55226249f216bd213ab", null ],
+    [ "GetPosition", "class_actor.html#a525317db6b9fa363529675f64f4f9e09", null ],
+    [ "HitTest", "class_actor.html#a439203b3447c327ead5c713e890891a9", null ],
+    [ "IsClickable", "class_actor.html#a5a1452bb2bc14c0cfeddbc7c345e331b", null ],
+    [ "IsEnabled", "class_actor.html#aaab6f23789199a7e6ada420f0be685dd", null ],
+    [ "operator=", "class_actor.html#a30db5f5b6d0db3dc169c18a471dc2af5", null ],
+    [ "SetClickable", "class_actor.html#ac44ae4cb8f1ece577b114f43a9149f50", null ],
+    [ "SetEnabled", "class_actor.html#a024a75b2735aa0531ba49b18d4256293", null ],
+    [ "SetPicture", "class_actor.html#ae8720ba8021438ac419e81587d6dc5f6", null ],
+    [ "SetPosition", "class_actor.html#a57acd820381fa72428e060886b1b27d4", null ],
+    [ "SetRoot", "class_actor.html#adcece4bc573ca5940de7647af30f317c", null ],
+    [ "mClickable", "class_actor.html#a42a6e1eae5514d18bf70c88e066fbeea", null ],
+    [ "mDrawablesInOrder", "class_actor.html#af6c44b9037324b91bef1798f7b600516", null ],
+    [ "mEnabled", "class_actor.html#a6995f75421cbfc1a24b9b445c3b73321", null ],
+    [ "mName", "class_actor.html#ab540465a3c2678a365c5ff96c247a666", null ],
+    [ "mPicture", "class_actor.html#a1e2a50ffe216401de6b829c14da0cb2b", null ],
+    [ "mPosition", "class_actor.html#a902ca17cbd3bedcbe2f8b9d796a71d0a", null ],
+    [ "mRoot", "class_actor.html#a814be6e414cf09d4fe63de9f7ac46b89", null ]
+];
---
+++ Step6/html/class_picture_1_1_iter.js
@@ -0,0 +1,9 @@
+var class_picture_1_1_iter =
+[
+    [ "Iter", "class_picture_1_1_iter.html#a7be15c610ee11858d7d3a1e57f540fa7", null ],
+    [ "operator!=", "class_picture_1_1_iter.html#a0e3851e8b1c5385ba646f896940bd0f7", null ],
+    [ "operator*", "class_picture_1_1_iter.html#a81d2dfeb4f7a3951cede8c0e0779be23", null ],
+    [ "operator++", "class_picture_1_1_iter.html#acfde098bb9abe8a542b3d0138a2e0df0", null ],
+    [ "mPicture", "class_picture_1_1_iter.html#a7033e465c3c14f9218463b70ba563d84", null ],
+    [ "mPos", "class_picture_1_1_iter.html#ae2ce3b38bd211a39a13df9fc92f93609", null ]
+];
